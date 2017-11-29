23e0e4d0de0599dab4cbd4d3f4030fc311305ce6
bthornto <brad@ipv4sub.net>
2017-11-29 15:16:58 -0800

    1.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='td4a',
-      version='1.2',
+      version='1.3',
       description='A browser based jinja template renderer',
       url='http://github.com/cidrblock/td4a',
       author='Bradley A. Thornton',
